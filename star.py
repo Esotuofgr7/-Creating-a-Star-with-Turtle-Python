09894811016f0f76246917e8c48c070a73bfec55
Esotuofgr7 <kachiesotu@gmail.com>
2025-08-02 18:17:58 +0100

    Add files via upload

---
+++ star.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+board = turtle.Turtle()
+
+# First Triangle for a Star
+board.forward(100) # Draw Base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+# Second Triangle for the Star
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
